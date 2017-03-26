84bd9db20319f7e4fce0584eb19ecd97aa76e205
ppammi <pammi.2000@gmail.com>
2017-03-26 15:17:00 +0100

    Create searchPageApi.jsx

---
+++ searchPageApi.jsx
@@ -0,0 +1,33 @@
+{
+  "user": null,
+  "success": true,
+  "data": [
+    {
+      "id": 187,
+      "apdId": "A004",
+      "productName": "US Commercial Paper (USCP)",
+      "qLProductName": null,
+      "primaryDesk": "Money Market Desk",
+      "productStatus": "",
+      "auditStatus": "Pending Confirmation",
+      "auditUser": "Guest",
+      "grouping1": null,
+      "grouping2": null,
+      "approvedVersionExists": false
+    },
+    {
+      "id": 6,
+      "apdId": "A006",
+      "productName": "Asset Backed US Commercial Paper (ABUSCP) ",
+      "qLProductName": null,
+      "primaryDesk": "Money Market Desk",
+      "productStatus": null,
+      "auditStatus": "Approved",
+      "auditUser": "Initial Data Setup",
+      "grouping1": null,
+      "grouping2": null,
+      "approvedVersionExists": false
+    }
+  ],
+  "errorMessages": []
+}
